6a6c4c16a499eb0f73b2d454a7571f068a97c73c
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-03-01 12:30:58 +0530

    Todo insert async working

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://admin@localhost:5432/todoapp'
@@ -20,9 +21,23 @@ def index():
     return render_template('index.html', data=Todo.query.all())
 
 @app.route("/todos/create", methods=['POST'])
-def create():
-    description = request.form.get('description')
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return redirect(url_for('index'))
+def create_todo():
+    error = False
+    body = {}
+    try:
+        # print(request.get_json())
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
