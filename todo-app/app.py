dcfb201793aee6483f00437e71a519f010cbaeda
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-03-04 23:01:54 +0530

    Added updating a todo functionality

--- todo-app/app.py
+++ todo-app/app.py
@@ -20,7 +20,7 @@ class Todo(db.Model):
 
 @app.route("/")
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 @app.route("/todos/create", methods=['POST'])
 def create_todo():
@@ -43,3 +43,16 @@ def create_todo():
         abort(400)
     else:
         return jsonify(body)
+
+@app.route("/todos/<todo_id>/set-completed", methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
