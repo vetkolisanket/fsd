10ed85bf044bd4696ce9a432f37ab795c9baef40
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-03-10 22:15:42 +0530

    Added route for get todos by todo id

--- todo-app/app.py
+++ todo-app/app.py
@@ -30,7 +30,11 @@ class TodoList(db.Model):
 
 @app.route("/")
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
+
+@app.route("/list/<list_id>")
+def get_list_todos(list_id):
+    return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
 
 @app.route("/todos/create", methods=['POST'])
 def create_todo():
