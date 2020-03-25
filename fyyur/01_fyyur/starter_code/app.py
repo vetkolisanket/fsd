9e69d92f8d067f2be7e76d6e38bfe77b9df02d11
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-03-25 18:43:52 +0530

    Fixed datetime error which was leading to crash on venues, artists and shows page

--- fyyur/01_fyyur/starter_code/app.py
+++ fyyur/01_fyyur/starter_code/app.py
@@ -63,11 +63,12 @@ class Artist(db.Model):
 
 def format_datetime(value, format='medium'):
   date = dateutil.parser.parse(value)
+  print(date)
   if format == 'full':
       format="EEEE MMMM, d, y 'at' h:mma"
   elif format == 'medium':
       format="EE MM, dd, y h:mma"
-  return babel.dates.format_datetime(date, format)
+  return babel.dates.format_datetime(date, format, locale='en_US')
 
 app.jinja_env.filters['datetime'] = format_datetime
 
